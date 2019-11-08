10964e44f8ff81467215783735179d4f0c1c43b9
TM <git@timsmanter.net>
2019-11-09 00:01:47 +0100

    Add path updating script

---
+++ update_paths.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# get absolute path of current directory
+wallpaperPath="$(cd "$(dirname "$0")"; pwd -P | perl -p -e 's/\//\\\//g')"
+
+# replace path with current absolute path
+perl -p -i -e "s/(<(file|from|to)>)\K.+(?=\/[\w-\.]+<\/(file|from|to)>)/$wallpaperPath/g" ./lakeside.xml
