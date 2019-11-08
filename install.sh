06962388c54a89ede73faa00d170e371ab2ea85c
TM <git@timsmanter.net>
2019-11-09 00:34:34 +0100

    Fix path issues

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo 'Updating paths...'
-./update_paths.sh
-
 # get absolute path of current directory
 wallpaperPath="$(cd "$(dirname "$0")"; pwd -P)"
+
+echo 'Updating paths...'
+$wallpaperPath/update_paths.sh
 
 echo 'Setting wallpaper...'
 gsettings set org.gnome.desktop.background picture-uri "file:///$wallpaperPath/lakeside.xml"
--- update_paths.sh
+++ update_paths.sh
@@ -4,4 +4,4 @@
 wallpaperPath="$(cd "$(dirname "$0")"; pwd -P | perl -p -e 's/\//\\\//g')"
 
 # replace path with current absolute path
-perl -p -i -e "s/(<(file|from|to)>)\K.+(?=\/[\w-\.]+<\/(file|from|to)>)/$wallpaperPath/g" ./lakeside.xml
+perl -p -i -e "s/(<(file|from|to)>)\K.+(?=\/[\w-\.]+<\/(file|from|to)>)/$wallpaperPath/g" $(dirname "$0")/lakeside.xml
